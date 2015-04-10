bc8ade21bbd687ddb684dd10bb9276d3897e6f10
Michal Woronowicz <michal.woronowicz@siili.fi>
2015-04-10 09:30:51 +0200

    init

--- ScalaImplicit.scala
+++ ScalaImplicit.scala
@@ -2,4 +2,34 @@ package scalaworkshop
 
 object ScalaImplicit extends App {
 
+  // implicit conversion function
+  implicit def stringToDSL(value: String): DSL = new DSL(value)
+
+  "Application" should "works" in {
+    println("is it working?")
+  }
+
+  import MyImplicits._
+
+  println(2 power 2)
+
 }
+
+class DSL(value: String) {
+  def should(v1: String) = this
+  def in(block: => Unit) = {
+    println("checking")
+    block
+    println("success")
+  }
+}
+
+// implicit class
+object MyImplicits {
+
+  implicit class Power(value: Int) {
+    def power(v1: Int) = math.pow(value, v1)
+  }
+
+}
+
